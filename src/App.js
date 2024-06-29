a7ec73f0a3e04902fe88a7829c7122f93e7f31cd
LindsayTayenne <154006361+LindsayTayn@users.noreply.github.com>
2024-06-29 11:40:48 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project is created by Lindsay Tayenne and is open sourced on {""}
-        <a
-          href="https://github.com/LindsayTayn/react-weather-app"
-          target="blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This project is created by Lindsay Tayenne and is open sourced on {""}
+          <a
+            href="https://github.com/LindsayTayn/react-weather-app"
+            target="blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
